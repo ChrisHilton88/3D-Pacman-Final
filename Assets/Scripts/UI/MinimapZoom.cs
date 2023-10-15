cc05e5a8be9d6df49cafb1e57a8176ce558dfdd7
ChrisHilton88 <christopherhilton88@gmail.com>
2023-10-15 12:01:14 +0300

    Built some functionality to have the minimap camera follow the player when zoomed in

--- Assets/Scripts/UI/MinimapZoom.cs
+++ Assets/Scripts/UI/MinimapZoom.cs
@@ -4,14 +4,21 @@ using UnityEngine.InputSystem;
 
 public class MinimapZoom : MonoBehaviour
 {
+    private int _minZoomLevel = 30, _maxZoomLevel = 50;     // Variables to let the camera follow the player depending on orthographic size
+    private int[] _zoomLevel = { 60, 50, 40, 30 };
+    private int _currentFOVIndex;
+
     private float _lerpDuration = 1f;
+
+    private bool _lerpComplete;
+
+    private readonly Vector3 _defaultCamPosition = new Vector3(0, 96.5f, -4f);
+
 
     PlayerInputActions _playerInputActions;
     Coroutine _lerpCoroutine;
     Camera _cam;
-
-    int[] _zoomLevel = { 60, 50, 40, 30 };
-    int _currentFOVIndex;
+    [SerializeField] private Transform _playerPos;
 
 
     void OnEnable()
@@ -22,12 +29,38 @@ public class MinimapZoom : MonoBehaviour
         _playerInputActions.UI.MinimapDefault.started += MinimapDefaultStarted;
     }
 
+    void OnDisable()
+    {
+        _playerInputActions.UI.MinimapZoom.performed -= MinimapZoomStarted;
+        _playerInputActions.UI.MinimapDefault.started -= MinimapDefaultStarted;
+        _playerInputActions.UI.Disable();
+    }
+
     void Start()
     {
         _cam = GetComponentInChildren<Camera>();
         _cam.orthographicSize = 60;
         _currentFOVIndex = 0;
         _lerpCoroutine = null;
+    }
+
+    void LateUpdate()
+    {
+        if (_cam.orthographicSize >= 30 && _cam.orthographicSize <= 50)
+        {
+            Vector3 playerPosition = _playerPos.position;
+            Vector3 cameraPosition = transform.position;
+
+            // Update the camera's global position to follow the player
+            cameraPosition.x = playerPosition.x;
+            cameraPosition.z = playerPosition.z;
+            transform.position = cameraPosition;
+        }
+        else
+        {
+            // Return the camera to its default global position when outside the specified range
+            transform.position = _defaultCamPosition;
+        }
     }
 
     #region Events
@@ -38,12 +71,14 @@ public class MinimapZoom : MonoBehaviour
             if (context.ReadValue<float>() == 1)        // Zoom in
             {
                 (float newFOV, float previousFOV) zoomValues = ZoomIn();
-                _lerpCoroutine = StartCoroutine(LerpThroughFOVS(zoomValues.previousFOV, zoomValues.newFOV));
+                _lerpCoroutine = StartCoroutine(LerpThroughFOVS(zoomValues.previousFOV, zoomValues.newFOV, transform.position, _playerPos.position));
+                Debug.Log("transform.position: " + transform.position);
+                Debug.Log("Player Pos: " + _playerPos.position);
             }
             else if (context.ReadValue<float>() == -1)      // Zoom out
             {
                 (float newFOV, float previousFOV) zoomValues = ZoomOut();
-                _lerpCoroutine = StartCoroutine(LerpThroughFOVS(zoomValues.previousFOV, zoomValues.newFOV));
+                _lerpCoroutine = StartCoroutine(LerpThroughFOVS(zoomValues.previousFOV, zoomValues.newFOV, transform.position, _playerPos.position));
             }
         }
         else
@@ -92,25 +127,23 @@ public class MinimapZoom : MonoBehaviour
     }
 
     // Smooth out the transition between FOV's
-    IEnumerator LerpThroughFOVS(float currentFOV, float targetFOV)
+    IEnumerator LerpThroughFOVS(float currentFOV, float targetFOV, Vector3 currentCameraPos, Vector3 targetCameraPos)
     {
         float interpolationProgress = 0;        // Starting time for progress
 
-        while(interpolationProgress < 1.0f) 
+        Vector3 initialCameraPosition = currentCameraPos;
+
+        while (interpolationProgress < 1.0f) 
         {
             interpolationProgress += Time.deltaTime / _lerpDuration;
             float newFOV = Mathf.Lerp(currentFOV, targetFOV, interpolationProgress);
             _cam.orthographicSize = newFOV;
+
+            Vector3 newPosition = new Vector3(targetCameraPos.x, initialCameraPosition.y, targetCameraPos.z);
+            transform.position = Vector3.Lerp(initialCameraPosition, newPosition, interpolationProgress);
             yield return null;      
         }
 
         _lerpCoroutine = null;
-    }
-
-    void OnDisable()
-    {
-        _playerInputActions.UI.MinimapZoom.performed -= MinimapZoomStarted;
-        _playerInputActions.UI.MinimapDefault.started -= MinimapDefaultStarted;
-        _playerInputActions.UI.Disable();
     }
 }
